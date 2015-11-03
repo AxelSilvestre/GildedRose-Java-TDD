0792e89ccfd692e6c36e2c87598524c4a78fd422
silvesta <silvesta@PC-B124-01.iut-valence.fr>
2015-11-03 13:41:53 +0100

    Red (SellIn under 0)

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -11,31 +11,48 @@ public class GildedRoseTest
 	@Test
 	public void testQualitySellIn()
 	{
-				
-		 ArrayList<Item> items = new ArrayList<Item>(); 
-		 
-		 items.add(new Item("MyItem1", 10, 20));
-		 items.add(new Item("MyItem2", 10, 20));
-		 items.add(new Item("MyItem3", 10, 20));
-		 items.add(new Item("MyItem4", 10, 20));
-		 
 
-		 for(int i=1; i<4; i++){
-			 GildedRose.updateItems(items);
-			 for(Item item : items){
-				 assertEquals(item.getSellIn(), 10-i);
-				 assertEquals(item.getQuality(), 20-i);
-			 }
-		 }
-		 
+		ArrayList<Item> items = new ArrayList<Item>(); 
+
+		items.add(new Item("MyItem1", 10, 20));
+		items.add(new Item("MyItem2", 10, 20));
+		items.add(new Item("MyItem3", 10, 20));
+		items.add(new Item("MyItem4", 10, 20));
+
+
+		for(int i=1; i<4; i++){
+			GildedRose.updateItems(items);
+			for(Item item : items){
+				assertEquals(item.getSellIn(), 10-i);
+				assertEquals(item.getQuality(), 20-i);
+			}
+		}
+
 	}
-	
+
+	@Test
+	public void testUnderSellIn(){		
+		
+		ArrayList<Item> items = new ArrayList<Item>(); 
+
+		items.add(new Item("MyItem1-2", 0, 20));
+
+
+		for(int i=2; i<8; i+=2){
+			GildedRose.updateItems(items);
+			for(Item item : items){
+				assertEquals(item.getQuality(), 20-i);
+			}
+		}
+		
+	}
+
 	@Test
 	public void testEmptyList(){
 		ArrayList<Item> items = new ArrayList<Item>(); 
-		 
+
 		GildedRose.updateItems(items);
-		
+
 		assertEquals(items.size(), 0);
 	}
 
